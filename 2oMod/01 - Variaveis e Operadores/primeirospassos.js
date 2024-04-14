d7f623cdbcc572eeb3c28a04d0bb012bb7cb54f5
Yagoo Pereira <yagoo.pereira@alphabeto.com>
2024-04-14 20:17:27 -0300

    Subindo Aula 1

---
+++ 2oMod/01 - Variaveis e Operadores/primeirospassos.js
@@ -0,0 +1,33 @@
+
+// let varA = 10;
+
+// const varB = 20;
+
+// console.log(varA);
+
+// varA = 20 + varA;
+
+// console.log(varA);
+
+// varA = 20 - varA;
+
+// console.log(varA);
+
+// varA = 20 * varA;
+
+// console.log(varA);
+
+// varA = 20 / varA;
+
+// console.log(varA);
+
+
+const varGasolina = 5.80;
+const varkmporlitro = 12;
+const varDistancia = 600;
+let varConsumo = 0;
+
+varConsumo = ((varDistancia / varkmporlitro) * varGasolina)
+
+console.log('O Valor da Viagem é: ' + varConsumo.toFixed(2))
+
