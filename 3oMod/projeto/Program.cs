33d51b652191c40f4cb4e0ad18f26401899ba509
Yagoo Pereira <yagoo.pereira@alphabeto.com>
2024-04-19 23:23:38 -0300

    novas aulas

--- 3oMod/projeto/Program.cs
+++ 3oMod/projeto/Program.cs
@@ -10,25 +10,135 @@
 
 
 
-string apresentacao = "Ola, seja bem vindo";
+// string apresentacao = "Ola, seja bem vindo";
 
-int quantidade = 1;
+// int quantidade = 1;
 
-double altura = 1.85;
+// double altura = 1.85;
 
-decimal preco = 1.80M;
+// decimal preco = 1.80M;
 
-bool condicao = true;
+// bool condicao = true;
 
-DateTime dataAtual = DateTime.Now;
+// DateTime dataAtual = DateTime.Now;
 
 
-Console.WriteLine(apresentacao);
-Console.WriteLine("O valor da quantidade: " + quantidade);
-Console.WriteLine("O valor da altra: " + altura);
-Console.WriteLine("O valor da preco: " + preco);
-Console.WriteLine("O valor da condicao: " + condicao);
-Console.WriteLine("Hoje é dia: " + dataAtual.ToString("dd/MM/yyyy"));
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("O valor da quantidade: " + quantidade);
+// Console.WriteLine("O valor da altra: " + altura);
+// Console.WriteLine("O valor da preco: " + preco);
+// Console.WriteLine("O valor da condicao: " + condicao);
+// Console.WriteLine("Hoje é dia: " + dataAtual.ToString("dd/MM/yyyy"));
+
+
+
+// int a = 10;
+// int b = 20;
+// int c = a + b;
+// c += + 5;
+// Console.WriteLine(c);
+
+
+
+// int quantidadeEmEstoque = 10;
+
+// int quantidadePedido = 4;
+
+// bool possivelVenda = quantidadeEmEstoque >= quantidadePedido;
+
+// if (quantidadePedido == 0)
+//     {
+//         Console.WriteLine("Venda Invalida");
+//     }
+// else if (possivelVenda)
+//     {
+//         Console.WriteLine("Venda pode ser Realizada");
+//     }
+// else
+//     {
+//         Console.WriteLine("Venda não pode ser Realizada");
+//     }
+
+
+
+// Console.WriteLine("Digite uma Letra");
+// String letra = Console.ReadLine();
+
+// if (letra == "a" ||
+//     letra == "e" ||
+//     letra == "i" ||
+//     letra == "o" ||
+//     letra == "u"
+//     )
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else
+// {
+//     Console.WriteLine("Não é uma vogal");
+// }
+
+
+
+
+// Console.WriteLine("Digite uma Letra");
+// String letra = Console.ReadLine();
+
+// switch (letra)
+// {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         Console.WriteLine("Vogal");
+//         break;
+
+//     default:
+//         Console.WriteLine("Não é uma vogal");
+//         break;
+// }
+
+
+
+
+// bool ehMaiorIdade = true;
+// bool possuiAutorizacao = false;
+
+// if (ehMaiorIdade || possuiAutorizacao)
+// {
+//     Console.WriteLine("Entrada Liberada")
+// }
+// else
+// {
+//     Console.WriteLine("Entrada não Liberada")
+// }
+
+
+bool possuiPresensaMinima = true;
+double media = 7.5;
+
+if (possuiPresensaMinima && media >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else
+{
+    Console.WriteLine("Reprovado")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
