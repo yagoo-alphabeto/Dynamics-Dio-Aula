7419f5d0c01fd5e8921ba330b3bea534041c9b2f
Yagoo Pereira <yagoo.pereira@alphabeto.com>
2024-04-19 22:20:56 -0300

    novos projetos

---
+++ 3oMod/projeto/Program.cs
@@ -0,0 +1,34 @@
+﻿using projeto.Models;
+
+
+// Pessoa p = new Pessoa();
+
+// p.Nome = "Yagoo";
+// p.Idade = 30;
+// p.Apresentar();
+
+
+
+
+string apresentacao = "Ola, seja bem vindo";
+
+int quantidade = 1;
+
+double altura = 1.85;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+DateTime dataAtual = DateTime.Now;
+
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("O valor da quantidade: " + quantidade);
+Console.WriteLine("O valor da altra: " + altura);
+Console.WriteLine("O valor da preco: " + preco);
+Console.WriteLine("O valor da condicao: " + condicao);
+Console.WriteLine("Hoje é dia: " + dataAtual.ToString("dd/MM/yyyy"));
+
+
+
